aecac77255f14264c635c689342769c0d07c78bd
lihengzhe <zeroli@gm99.com>
2023-03-20 13:55:28 +0800

    Anime

--- notion.php
+++ notion.php
@@ -36,7 +36,7 @@ foreach ($result['data']['results'] as $k => $row) {
     $data[$k]['译名'] = $notion->fieldShow($row['properties']['译名']);
     $data[$k]['进度'] = $notion->fieldShow($row['properties']['进度']);
     $data[$k]['编辑'] = $notion->fieldShow($row['properties']['最近编辑']);
-    $data[$k]['操作'] = '<a href="javascript:;" class="btn btn-prev" data-old="' . $data[$k]['进度'] . '" data-id="' . $row['id'] . '">减一</a><div class="ONEREM"></div><a href="javascript:;" class="btn btn-next" data-old="' . $data[$k]['进度'] . '" data-id="' . $row['id'] . '">加一</a>';
+    $data[$k]['操作'] = '<a href="javascript:;" class="btn btn-prev" data-old="' . $data[$k]['进度'] . '" data-id="' . $row['id'] . '">减一</a>&nbsp;&nbsp;<a href="javascript:;" class="btn btn-next" data-old="' . $data[$k]['进度'] . '" data-id="' . $row['id'] . '">加一</a>';
     $data[$k]['推荐度'] = $notion->fieldShow($row['properties']['推荐度']);
     $data[$k]['更新'] = $notion->fieldShow($row['properties']['更新']);
     $data[$k]['特别篇'] = $notion->fieldShow($row['properties']['特别篇']);
