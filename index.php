842a26dbee32e82744903032175811c32a69f2dd
lihengzhe <zeroli@gm99.com>
2023-03-20 17:51:59 +0800

    Anime

--- index.php
+++ index.php
@@ -1,7 +1,3 @@
 <?php
-session_start();
-if(empty($_SESSION['admin']['userid']) && $_SERVER['REMOTE_ADDR'] != '127.0.0.1') {
-    header('Location:/zero');
-    exit;
-}
-require_once('anime.php');
+require_once 'Common.php';
+require_once 'anime.php';
