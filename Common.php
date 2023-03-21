2246e448fbc40411fdff82e2aea2a93d69b491d0
lihengzhe <zeroli@gm99.com>
2023-03-21 11:37:56 +0800

    Anime

--- Common.php
+++ Common.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 if(empty($_SESSION['admin']['userid']) && $_SERVER['REMOTE_ADDR'] != '127.0.0.1') {
-    header('Location:/zero');
+    header('Location:/zero?forward=/anime');
     exit;
 }
 require_once 'config.php';
